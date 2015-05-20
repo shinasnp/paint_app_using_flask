8e3b67290495773e65fdf6e786a74c4b6280afd7
shinasnp <22shinas@gmail.com>
2015-05-20 23:38:09 +0530

    eighth commit

---
+++ test.py
@@ -0,0 +1,48 @@
+from flask import Flask,request,url_for,render_template,redirect,Response
+import sqlite3
+import json
+app = Flask(__name__)
+@app.route("/")
+def home():
+	return render_template('paint.html')
+@app.route('/gallery/<imagename>', methods=['POST', 'GET'])
+def mainpage(imagename=None):
+    if request.method == 'GET':
+        if imagename:
+            imgname=(imagename,)
+            con = sqlite3.connect('Image.db')
+            cur = con.cursor()
+            cur.execute("SELECT * FROM Image WHERE imgname=?", imgname)
+            rows = cur.fetchall()
+            if rows:
+                for row in rows:
+                    imgname = row[0]
+                    imgdata = row[1]
+                return render_template('paint.html', saved=imgdata)
+            else:
+                resp = Response("""<html> <script> alert("Image not found");document.location.href="/" </script> </html>""")
+                return resp
+        else:
+            return render_template('paint.html')
+    if request.method == 'POST':
+        imgname=request.form['imagename']
+        imgdata=request.form['string']
+	data=(imgname, imgdata)
+	con = sqlite3.connect("Image.db")
+        cur=con.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS Image(imgname text, imgdata string)")
+        cur.execute("INSERT INTO Image VALUES(?, ?)", data)
+        con.commit()
+        con.close()
+        resp = Response("saved")
+        return resp
+@app.route('/gallery')
+def gallery():
+	con = sqlite3.connect("Image.db")
+        cur=con.cursor()
+        cur.execute("SELECT * FROM Image ORDER BY imgname")
+	posts=[dict(id=i[0],title=i[1]) for i in cur.fetchall()]	
+	return render_template('gallery.html',posts=posts)
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
